094b559ddeeead306a47e624b1d777c5dbea5ee7
Phung Dam Tien Si <siphung953@gmail.com>
2026-09-07 03:29:49 +0700

    refactor(public): remove unused field

--- apps/api/src/api-external/job-discovery/JobListItemDto.ts
+++ apps/api/src/api-external/job-discovery/JobListItemDto.ts
@@ -3,7 +3,6 @@ import { EmploymentType } from '../../api-shared/type/EmploymentType';
 export interface JobListItemDto {
     id: string;
     title: string;
-    description?: string;
     employmentType: EmploymentType;
     location: string;
 }
